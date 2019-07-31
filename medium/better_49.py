df19ae2e5d1e0752b1628e3e53a80035e239a0eb
fanjie shen <1506092069@qq.com>
2019-07-31 15:53:58 +0800

    Update better_49.py

--- medium/better_49.py
+++ medium/better_49.py
@@ -3,5 +3,5 @@ class Solution:
 	    d = {}
 	    for w in sorted(strs):
 	        key = tuple(sorted(w))
-	        d[key] = d.get(key, []) + [w]
+	        d[key] = d.get(key, []) + [w]	#get(key, default_value) default value is take place when no value in dict[key]
 	    return d.values()
