e5a64cba85f83137dc021532bc5ed060d10b0cc6
fanjie shen <1506092069@qq.com>
2019-08-05 18:40:38 +0800

    Update 91.py

--- medium/91.py
+++ medium/91.py
@@ -7,9 +7,9 @@ class Solution:
         dp[0] = 1
         dp[1] = 1 if 0 < int(s[0]) <= 9 else 0
 
-        for i in range(2, len(s) + 1):
+        for i in range(2, len(s) + 1):  #两可能
             if 0 < int(s[i-1:i]) <= 9:
                 dp[i] += dp[i - 1]
             if s[i-2] != '0' and int(s[i-2:i]) <= 26:
                 dp[i] += dp[i - 2]
-        return dp[len(s)]
+        return dp[-1]
