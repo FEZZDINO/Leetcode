2d7d73865f809b5600abe8e2335359ec28a9c492
fanjie shen <1506092069@qq.com>
2019-08-09 17:01:01 +0800

    Update 11.py

--- medium/11.py
+++ medium/11.py
@@ -4,13 +4,13 @@ class Solution:
         left, right = 0, len(height) - 1
         while left < right:
             l, r = height[left], height[right]
-            if l < r:
+            if l < r:                       
                 area = (right - left) * l
-                while height[left] <= l:
+                while height[left] <= l:        #减少runtime
                     left += 1
             else:
                 area = (right - left) * r
-                while height[right] <= r and right:
+                while height[right] <= r and right: #减少runtime
                     right -= 1
             if area > max_area:
                 max_area = area
