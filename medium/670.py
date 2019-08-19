460736da545e03fb34adfa230f47779a0d2fa5e3
fanjie shen <1506092069@qq.com>
2019-08-19 11:45:31 +0800

    670

---
+++ medium/670.py
@@ -0,0 +1,26 @@
+class Solution:
+    def maximumSwap(self, num: int) -> int:
+        str_num = [int(x) for x in str(num)]
+        
+        for i in range(0, len(str_num)-1):
+            ma = max(str_num[i:])
+            if str_num[i] != ma:
+                index = len(str_num) - str_num[::-1].index(max(str_num[i:])) -1
+                str_num[i], str_num[index] = str_num[index], str_num[i]
+                return int("".join(map(str, str_num)))
+        
+        return num
+        
+        
+        
+        '''
+        str_num = [int(x) for x in str(num)]
+        for i in range(0, len(str_num)-1):
+            if str_num[i+1] >= str_num[i]:
+                #index = len(str_num) - str_num[::-1].index(max(str_num[i:])) -1
+                index = str_num.index(max(str_num[i:]))
+                str_num[i], str_num[index] = str_num[index], str_num[i]
+                return int("".join(map(str, str_num)))
+        return num
+        找到从末尾开始，至当前位置+1的最大数，并将之与当前位置的数交换
+        '''
