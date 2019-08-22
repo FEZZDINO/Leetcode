7af06a812e511997cd7bdf8fe713bb464c6eb485
fanjie shen <1506092069@qq.com>
2019-08-22 14:01:05 +0800

    394

--- medium/394.py
+++ medium/394.py
@@ -4,15 +4,15 @@ class Solution:
         stack.append(["", 1])
         num = ""
         for ch in s:
-            if ch.isdigit():
-                num += ch
+            if ch.isdigit():        #"100"would be split as "1", "0", "0"
+                num += ch        
             elif ch == '[':
                 stack.append(["", int(num)])
                 num = ""
             elif ch == ']':
-                st, k = stack.pop()
-                stack[-1][0] += st*k
-            else:
+                st, k = stack.pop()     #from the last of stack
+                stack[-1][0] += st*k    #stack[-1][0] is str
+            else:                       #str after[], without number
                 stack[-1][0] += ch
             #print(stack)
         return stack[0][0]
